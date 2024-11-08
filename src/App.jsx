00f066adf6902d95cc45e2b7949a84bd2acef6c7
Ngọc <tojimomirudo54@gmail.com>
2024-11-08 23:52:45 +0700

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,62 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+//truyền thôngd => arrow function
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   // function myFunction() {
+//   //   console.log(">>>run my function");
+//   // }
+
+//   //arrow function
+//   const myFunction = () => {
+//     //body
+//     console.log(">>>run my arrow function");
+//   };
+//   myFunction();
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Hello world 1</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   );
+// }
+
+// () => { }
+const App = () => {
   const [count, setCount] = useState(0);
+
+  // function myFunction() {
+  //   console.log(">>>run my function");
+  // }
+
+  //arrow function
+  const myFunction = () => {
+    //body
+    console.log(">>>run my arrow function");
+  };
+  myFunction();
 
   return (
     <>
@@ -30,6 +84,6 @@ function App() {
       </p>
     </>
   );
-}
+};
 
 export default App;
